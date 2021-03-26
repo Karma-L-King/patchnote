5a7e9baae3ff5a07ffc40df8ce081681491d29e1
Karma King <2114747@talnet.nl>
2021-03-26 17:25:34 +0100

    nieuwe add story pagina gemaakt

---
+++ jquery/add-note-to-story.js
@@ -0,0 +1,14 @@
+$('.add-more').click(function() {
+    var clone = $('.form-main').clone();
+   
+ 
+ 
+    
+    $('.form-main').append(clone);
+    
+  });
+
+  /// dupliceert 1>2>4>8 
+  ///doel per 1 
+  ///////
+ 
